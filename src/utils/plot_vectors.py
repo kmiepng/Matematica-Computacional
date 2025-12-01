38794a433fe1dc07014968c9574b98aee757cdc3
Kamila Sarmanho <kamilasarmanho@gmail.com>
2025-11-30 21:30:56 -0300

    feat: add determinant to plot_vectors

--- src/utils/plot_vectors.py
+++ src/utils/plot_vectors.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_vectors(title, vectors):
+def plot_vectors(title, vectors, determinant = False):
     plt.figure(figsize=(6, 6))
     plt.axhline(0, color='black', linewidth=1)
     plt.axvline(0, color='black', linewidth=1)
@@ -14,6 +14,15 @@ def plot_vectors(title, vectors):
         pos_x = x + 0.1
         pos_y = y + 0.1 if y >= 0 else y - 0.4
         plt.text(pos_x, pos_y, f"({x}, {y})", fontsize=12, color=colors[i])
+
+    if determinant:
+        det_val = np.linalg.det(vectors)
+        area = abs(det_val)
+
+        parallelogram = np.array([[0, 0], vectors[0], vectors[0] + vectors[1], vectors[1]])
+
+        plt.fill(parallelogram[:, 0], parallelogram[:, 1], color='red', alpha=0.2, label=f'det = {area:.2f}')
+        plt.legend()
 
     plt.xlim(-5, 5)
     plt.ylim(-5, 5)
